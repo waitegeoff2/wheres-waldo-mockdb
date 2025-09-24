35779c0d2146b2acbe63e4e869e79fa6cfa3fff0
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-09-24 15:49:46 -0400

    Order high scores by order of lowest time first

--- frontend/src/components/HighScoreList/HighScoreList.jsx
+++ frontend/src/components/HighScoreList/HighScoreList.jsx
@@ -9,23 +9,31 @@ export default function HighScoreList() {
 
     console.log(highScoreData)
 
+    //sorting the arrays by lowest score first
+    const sortedTownArray = topTownScores.sort((a, b) => (a.score - b.score))
+    const sortedBeachArray = topBeachScores.sort((a, b) => (a.score - b.score))
+    const sortedConventionArray = topConventionScores.sort((a, b) => (a.score - b.score))
+    console.log(sortedBeachArray)
+
+
     return (
+        // run it on the sorted arrays above
         <>
         <div className="high-score-section">
             <h1>High Scores</h1>
             <ol className="high-score-list">
                 {selectedPhoto===2 && 
-                    topTownScores.map((score, index) => (
+                    sortedTownArray.map((score, index) => (
                     <b><li className="high-score-list-item">{ score.name }: { score.score } seconds.</li></b> 
                     ))
                 }
                 {selectedPhoto===3 && 
-                    topBeachScores.map((score, index) => (
+                    sortedBeachArray.map((score, index) => (
                     <b><li className="high-score-list-item">{ score.name }: { score.score } seconds.</li></b> 
                     ))
                 }
                 {selectedPhoto===4 && 
-                    topConventionScores.map((score, index) => (
+                    sortedConventionArray.map((score, index) => (
                     <b><li className="high-score-list-item">{ score.name }: { score.score } seconds.</li></b> 
                     ))
                 }
